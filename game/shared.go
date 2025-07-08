f74d77e0b9b9e76f25d97d2ef03456f9a5a58ddd
WRABZY <wrabzy@pm.me>
2025-07-08 19:43:01 +0300

    Entity codes have been adjusted

--- game/shared.go
+++ game/shared.go
@@ -1,20 +1,20 @@
 package catch
 
-// entity codes
+// Entity codes
 const (
-	PLAYER = -1
 	EMPTY  = 0
-	PAW    = 1
-	ROOK   = 3 // 2 and 3
-	BISHOP = 6 // 4, 5 and 6
-	// reserved, add in future? KNIGHT = 10 // 7, 8, 9 and 10
-	QUEEN         = 15 // 11, 12, 13, 14, 15
-	KING          = 16
-	SHADOW_PLAYER = 17
-	SHADOW_PAW    = 18
-	SHADOW_ROOK   = 19
-	SHADOW_BISHOP = 20
-	// reserved, add in future? SHADOW_KNIGHT = 21
-	SHADOW_QUEEN = 22
-	SHADOW_KING  = 23
+	PLAYER = 1 << iota
+	PAW
+	ROOK
+	BISHOP
+	KNIGHT
+	QUEEN
+	KING
+	SHADOW_PLAYER
+	SHADOW_PAW
+	SHADOW_ROOK
+	SHADOW_BISHOP
+	SHADOW_KNIGHT
+	SHADOW_QUEEN
+	SHADOW_KING
 )
